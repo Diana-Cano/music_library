27dbf1ae63f18b152a676f1f8fe62093fb4488b6
Diana-Cano <dianacanoo@icloud.com>
2023-01-16 20:28:56 -0800

    use ref hook

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,20 +1,52 @@
-import { useContext } from 'react'
-import { DataContext } from '../context/DataContext'
-import GalleryItem from './GalleryItem'
+import { useState } from 'react' 
 
-function Gallery(){
-    const data = useContext(DataContext)
+function GalleryItem(props) {
+    let [view, setView] = useState(false)
+
+    const simpleStyle = {
+        'width': '25vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px'
+    }
     
-    const display = data.map((item,index) => {
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
+
+    const simpleView = () => {
         return (
-            <GalleryItem item={item} key={index} />
+            <div style={simpleStyle}>
+                <h3>{props.item.trackName}</h3>
+                <h4>{props.item.collectionName}</h4>
+            </div>
         )
-    })
+    }
+
+    const detailView = () => {
+        return (
+            <div style={detailStyle}>
+                <h2>{props.item.trackName}</h2>
+                <h3>{props.item.collectionName}</h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            {display}
+        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
+            <p>One Gallery Item</p>
+                {view ? detailView() : simpleView()}
         </div>
     )
 }
 
-export default Gallery
+export default GalleryItem
